c78c2d58a03639db15a001d2f62952eb78d131c1
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-11-24 16:10:54 +0100

    feat: began to make 3d environment with lighting and implementing 3d models

--- src/sections/Room.jsx
+++ src/sections/Room.jsx
@@ -1,13 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import Piano from "../components/models/Piano_animation";
+import MusicRoom from "../components/models/Optimized-room";
+import Lights from "../components/lights/Lights";
 
 function Room() {
-	return (
-		<div style={{ width: "100vw", height: "100vh" }}>
-			<Canvas camera={{ position: [2, 2, 5] }}>
-				<OrbitControls />
-			</Canvas>
-		</div>
-	);
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [5, 1,5] }}>
+        <OrbitControls  
+          target={[3, 1, 3]}    
+        />
+        <Lights />
+        <MusicRoom />
+        <MusicRoom position={[0, 0, 6.27]} rotation={[0, Math.PI / 2, 0]} />
+        <MusicRoom position={[6.27, 0, 6.27]} rotation={[0, Math.PI, 0]} />
+        <MusicRoom position={[6.27, 0, 0]} rotation={[0, 3.5*Math.PI, 0]}/>
+        <Piano position={[0.2, 0.81, -2.3]} />
+      </Canvas>
+    </div>
+  );
 }
+
 export default Room;
