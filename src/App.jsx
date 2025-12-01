74e6a89eaadefc2a1f7e049873b118bce345c082
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-12-01 17:30:28 +0100

    feat: refine HomeSection layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import Room from "./sections/Room";
 import HomeSection from "./sections/HomeSection";
-import "./index.css"
+import "./index.css";
 function App() {
-
 	return (
 		<>
-			{/* <Room /> */}
 			<HomeSection />
+			{/* <Room /> */}
 		</>
 	);
 }
