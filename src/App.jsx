92dca5f2d4e4571d750d2bce118a67100d551346
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-12-01 20:10:53 +0100

    feat: add AboutSection component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import Room from "./sections/Room";
 import HomeSection from "./sections/HomeSection";
 import "./index.css";
+import AboutSection from "./sections/AboutSection";
 function App() {
 	return (
 		<>
 			<HomeSection />
+			<AboutSection/>
 			{/* <Room /> */}
 		</>
 	);
---
+++ src/sections/AboutSection.jsx
@@ -0,0 +1,10 @@
+function AboutSection() {
+	return (
+		<section className="about-section overflow-hidden min-h-screen flex items-center justify-center">
+			<div className="p-10 text-center">
+				<h1>Hey ! I'm Matteo and I have a passion for music since I was a little baby</h1>
+			</div>
+		</section>
+	);
+}
+export default AboutSection;
