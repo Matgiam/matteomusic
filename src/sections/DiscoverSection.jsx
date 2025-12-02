5034013e7612489d72a6bb076a2f0b84f168404e
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-12-02 22:36:15 +0100

    feat: enhance DiscoverSection with GSAP animations - Integrated GSAP animations for text elements in DiscoverSection. - Updated text structure for improved styling and animation effects.

--- src/sections/DiscoverSection.jsx
+++ src/sections/DiscoverSection.jsx
@@ -1,13 +1,69 @@
+import { useGSAP } from "@gsap/react";
 import GlobeComponent from "../components/GlobeComponent";
+import gsap from "gsap";
 
 function DiscoverSection() {
+	useGSAP(() => {
+		gsap.to(".first", {
+			xPercent: "80",
+			rotate: "30deg",
+			padding: "0.8em",
+			scrollTrigger: {
+				trigger: ".discover-section",
+				start: "-20% 10%",
+				end: "50% top",
+				markers: true,
+				scrub: true,
+			},
+		});
+		gsap.to(".second", {
+			rotate: "-30deg",
+			xPercent: "-50",
+			padding: "0.8em",
+			scrollTrigger: {
+				trigger: ".discover-section",
+				start: "-15% 10%",
+				end: "50% top",
+				markers: true,
+				scrub: true,
+			},
+		});
+
+		gsap.to(".third", {
+			xPercent: "90",
+			rotate: "30deg",
+			padding: "0.8em",
+			scrollTrigger: {
+				trigger: ".discover-section",
+				start: "-5% 10%",
+				end: "50% top",
+				markers: true,
+				scrub: true,
+			},
+		});
+
+		gsap.to(".fourth", {
+			rotate: "-30deg",
+			padding: "0.8em",
+			scrollTrigger: {
+				trigger: ".discover-section",
+				start: "-10% 10%",
+				end: "50% top",
+				markers: true,
+				scrub: true,
+			},
+		});
+	});
 	return (
 		<section className="discover-section">
 			<div className="flex justify-around items-center h-full">
 				<div className="flex-col ml-100 text-center">
-					<h1>Want</h1>
-					<h2>to discover</h2>
-					<h2>my world ? </h2>
+					<div className="text">
+						<h1 className="first">Want</h1>
+						<h2 className="second">to discover</h2>
+						<h2 className="third">my </h2>
+						<h2 className="fourth">world ? </h2>
+					</div>
 				</div>
 
 				<GlobeComponent />
